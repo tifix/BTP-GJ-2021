b94b3488ba72cc75b365ff187cbff5c9aac14f6f
Tifix <wertynius2@gmail.com>
2021-01-23 12:50:57 +0000

    Introduced Player health and passive healing when out of combat
    Throw playerhealth onto the player and now you can kill him. Awesome!
    
    Debug utility - aserisk to take damage

---
+++ UntitledGameJamGame/Assets/Scripts/Damageable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Base class for all damagable entities - enemies, player and all
+ * 
+ */
+
+public class Damagable : MonoBehaviour
+{
+    public float HP;
+    public float max_HP;
+
+
+    public virtual void TakeDamage(float damage)
+    {
+        if (HP - damage <= 0) Destroy(gameObject);
+        else HP -= damage;
+    }
+
+    public void Heal(float amount)
+    {
+        if (HP + amount >= max_HP) HP=max_HP;
+        else HP += amount;
+    }
+
+}
